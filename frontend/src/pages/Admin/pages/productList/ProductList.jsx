3298e448ccd51816613a4e842515b83a29c0db9c
Priyanshu Kapadia <priyanshu.kapadia@intesols.com.au>
2024-04-03 17:50:48 +0530

    Product popup

--- frontend/src/pages/Admin/pages/productList/ProductList.jsx
+++ frontend/src/pages/Admin/pages/productList/ProductList.jsx
@@ -1,5 +1,5 @@
 import "./productList.css";
-import { DeleteOutline, Edit } from "@material-ui/icons";
+import { DeleteOutline, Edit, RemoveRedEye } from "@material-ui/icons";
 // import { productRows } from "../../dummyData";
 // import { Link } from "react-router-dom";
 import { useCallback, useEffect, useState } from "react";
@@ -7,11 +7,17 @@ import { DataGrid } from "@material-ui/data-grid";
 import { BASE_URL } from "../../../../utils/config";
 import { Spinner } from "reactstrap";
 import { Link } from "react-router-dom";
+import { Modal } from "@material-ui/core";
 
 export default function ProductList() {
   const token = localStorage.getItem("token");
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [tourData, setTourData] = useState();
 
   const fetchTours = useCallback(async () => {
     try {
@@ -108,6 +114,12 @@ export default function ProductList() {
       renderCell: (params) => {
         return (
           <>
+            <RemoveRedEye
+              onClick={() => {
+                handleOpen();
+                setTourData(params.row);
+              }}
+            />
             <Link to={"/admin/product/" + params.row._id}>
               {/* <div>
                 <button className="productListEdit"></button>
@@ -149,6 +161,67 @@ export default function ProductList() {
           getRowId={(row) => row._id}
         />
       </div>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 810,
+            backgroundColor: "white",
+            color: "white",
+            border: "2px solid grey",
+            boxShadow: "24px",
+            padding: "4px",
+            borderRadius: "10px",
+          }}
+        >
+          <div
+            style={{
+              height: "300px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <img
+              src={tourData?.photo}
+              alt="tour-img"
+              height={300}
+              // width={"100%"}
+            />
+          </div>
+          <div className="tour__info">
+            <h2>{tourData?.title}</h2>
+            <div className="tour__extra-details">
+              <span>
+                <i class="ri-map-pin-2-line"></i> {tourData?.city}
+              </span>
+              <span>
+                <i class="ri-money-dollar-circle-line"></i> $
+                {tourData?.Adult_Price} /For Adult(12+ years)
+              </span>
+              <span>
+                <i class="ri-money-dollar-circle-line"></i> $
+                {tourData?.Child_Price} /For Childern
+              </span>
+
+              <span>
+                <i class="ri-group-line"></i> {tourData?.maxGroupSize} people
+              </span>
+            </div>
+            <h5>Description</h5>
+            <p>{tourData?.desc}</p>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 }
